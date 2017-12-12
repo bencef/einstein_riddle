74140d50b666dbf01be5474846b669c1f5e852a2
Bence Fabian <begnoc@gmail.com>
2017-12-12 15:57:48 +0100

    move solution into a function

--- src/sherlock/core.clj
+++ src/sherlock/core.clj
@@ -30,21 +30,22 @@
                                   (partition 2
                                              (interleave rest-of-keys lvars)))))))))
 
-(run 1 [q]
-  (fresh [h1 h2 h3 h4 h5]
-    (== q [h1 h2 h3 h4 h5])
-    (everyg houseo q)
-    ;; TODO house numbers are distinct
-    (partiale q {:occupant :brit :color :red})
-    (partiale q {:occupant :swede :pet :dog})
-    (partiale q {:occupant :dane :drink :tea})
-    (fresh [n1 n2]
-      (fd/+ 1 n1 n2)
-      (everyg #(fd/in % (fd/interval 1 5)) [n1 n2])
-      (partiale q {:number n1 :color :green})
-      (partiale q {:number n2 :color :white}))
-    (partiale q {:color :green :drink :coffee})
-    (partiale q {:cigarette :pall-mall :pet :bird})
-    (partiale q {:color :yellow :cigarette :dunhill})
-    (partiale q {:number 3 :drink :milk})
-    (partiale q {:occupant :norwegian :number 1})))
+(defn solve []
+  (run 1 [q]
+    (fresh [h1 h2 h3 h4 h5]
+      (== q [h1 h2 h3 h4 h5])
+      (everyg houseo q)
+      ;; TODO house numbers are distinct
+      (partiale q {:occupant :brit :color :red})
+      (partiale q {:occupant :swede :pet :dog})
+      (partiale q {:occupant :dane :drink :tea})
+      (fresh [n1 n2]
+        (fd/+ 1 n1 n2)
+        (everyg #(fd/in % (fd/interval 1 5)) [n1 n2])
+        (partiale q {:number n1 :color :green})
+        (partiale q {:number n2 :color :white}))
+      (partiale q {:color :green :drink :coffee})
+      (partiale q {:cigarette :pall-mall :pet :bird})
+      (partiale q {:color :yellow :cigarette :dunhill})
+      (partiale q {:number 3 :drink :milk})
+      (partiale q {:occupant :norwegian :number 1}))))
